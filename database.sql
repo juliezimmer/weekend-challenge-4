51da6aa0d180d143286e20691b672cbe3bcacdd8
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-08-18 17:15:50 -0500

    basic skeleton of project set up

---
+++ database.sql
@@ -0,0 +1,8 @@
+create table employees (
+	id serial primary key,
+	first_name varchar (30) not null,
+	last_name varchar (40) not null,
+	job_title varchar (40) not null,
+	annual_salary integer not null
+);
+	
